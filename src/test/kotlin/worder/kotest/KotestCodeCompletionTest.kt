8c39c7cdaa71606d4a359359d31b12034d80cbb6
Yevhenii Nadtochii <nadto4y.evgeny@gmail.com>
2020-07-03 22:28:43 +0300

    updateSourceFileStamps task forced only for srs dir

---
+++ src/test/kotlin/worder/kotest/KotestCodeCompletionTest.kt
@@ -0,0 +1,9 @@
+package worder.kotest
+
+import io.kotest.core.spec.style.ShouldSpec
+
+class KotestCodeCompletionTest : ShouldSpec({
+    should("auto complete code with suggestions from Kotest API") {
+//        listOf("").shoul
+    }
+})
